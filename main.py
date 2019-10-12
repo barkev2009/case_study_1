49c103e73c64ae292c0b5e6262d544270156a9dd
kapinosanna <kapinosav@yandex.ru>
2019-10-12 15:26:03 +0700

    Initial commit.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Case 1
 
 def triangle():
-    # TODO: (Anna) 
+    # TODO: (Anna)
     pass
 
 def square():
@@ -11,3 +11,31 @@ def square():
 def rombus():
     # TODO: (Greg)
     pass
+
+from turtle import *
+
+speed(1)
+pensize(2)
+color('grey','pink')
+begin_fill()
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+end_fill()
+
+penup()
+goto(-100,0)
+pendown()
+setheading(45)
+forward(100)
+left(120)
+forward(100)
+left(120)
+forward(100)
+hideturtle()
+
+done()
